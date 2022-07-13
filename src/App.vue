25636d2f21f963f740219fa5b6a0479cad0c5d6b
thisisSsozi <ssozi.mr@gmail.com>
2022-07-13 08:30:06 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <HomePage />
+  </div>
+</template>
+
+<script>
+//mport HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage, 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1 id="myHeading" >
+            This is the HomePage.vue component
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomePage',
+    data(){},
+    methods:{},
+}
+</script>
+
+<style>
+    #myHeading{
+        color: purple;
+    }
+
+</style>
