9c03aaede7aeda6571e25780d9e5b896c2d762d3
thisisSsozi <ssozi.mr@gmail.com>
2022-07-15 09:43:35 +0300

    modified the course details

--- src/components/CoursesSection.vue
+++ src/components/CoursesSection.vue
@@ -28,9 +28,9 @@
               <div class="card-body">
                 <h6 class="card-title">HTML & CSS</h6>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+                  HTML is the language in which most websites are written. HTML is used to create pages and make them functional.
+                The code used to make them visually appealing is known as CSS and we shall focus on this in a later tutorial.
+                </p><br>
                 <!-- <a href="#" class="btn btn-dark p-1">_</a> -->
               </div>
             </div>
@@ -45,9 +45,8 @@
               <div class="card-body">
                 <h6 class="card-title">NODE JS</h6>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+                  Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser, which was designed to build scalable network applications. 
+                </p><br>
                 <!-- <a href="#" class="btn btn-dark p-1">_</a> -->
               </div>
             </div>
@@ -58,9 +57,8 @@
               <div class="card-body">
                 <h6 class="card-title">GIT</h6>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+                  Git is software for tracking changes in any set of files, usually used for coordinating work among programmers collaboratively developing source code during software development.
+                </p><br><br>
                 <!-- <a href="#" class="btn btn-dark p-1">_</a> -->
               </div>
             </div>
@@ -75,9 +73,8 @@
               <div class="card-body">
                 <h6 class="card-title">REACT</h6>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+                  React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies.
+                </p><br><br>
                 <!-- <a href="#" class="btn btn-dark p-1">_</a> -->
               </div>
             </div>
@@ -92,8 +89,7 @@
               <div class="card-body">
                 <h6 class="card-title">JAVASCRIPT</h6>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.
                 </p>
                 <!-- <a href="#" class="btn btn-dark p-1">_</a> -->
               </div>
@@ -109,8 +105,7 @@
               <div class="card-body">
                 <h6 class="card-title">JQUERY</h6>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation, as well as event handling, CSS animation, and Ajax.
                 </p>
                 <!-- <a href="#" class="btn btn-dark p-1">_</a> -->
               </div>
@@ -126,9 +121,8 @@
               <div class="card-body">
                 <h6 class="card-title">PYTHON</h6>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+                  Python is a high-level, interpreted, general-purpose programming language. 
+                </p><br>
                 <!-- <a href="#" class="btn btn-dark p-1">_</a> -->
               </div>
             </div>
@@ -139,8 +133,7 @@
               <div class="card-body">
                 <h6 class="card-title">SQL</h6>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  SQL is a domain-specific language used in programming and designed for managing data held in a relational database management system
                 </p>
                 <!-- <a href="#" class="btn btn-dark p-1">_</a> -->
               </div>
